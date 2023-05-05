1eef8c65035e50b031536629bae32e2af4404498
NEWUSER <chasevriend84@gmail.com>
2023-05-05 11:07:02 -0700

    new commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
+
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
+
+navLink.addEventListener('click', () => {
+    navLink.classList.toggle('hide');
+  });
+
