192a7add43f52b3715b378f91d70f239a2e945f0
Chase <chasevriend84@gmail.com>
2023-06-13 09:43:13 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav__link');
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
 
-hamburger.addEventListener('click', () => {
-  navLink.classList.toggle('hide');
-});
+  burger.addEventListener('click', () => {
+      // Toggle Nav
+      nav.classList.toggle('nav-active');
 
-navLink.addEventListener('click', () => {
-    navLink.classList.toggle('hide');
+      // Animate Links
+      navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+              link.style.animation = '';
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+          }
+      });
+
+      // Burger Animation
+      burger.classList.toggle('toggle');
   });
+};
 
+navSlide();
